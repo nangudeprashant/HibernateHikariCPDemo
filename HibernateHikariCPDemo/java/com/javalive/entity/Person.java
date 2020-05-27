107ca91f0aeacd452fc914a72514282693bc4f47
Administrator <Administrator@DELL>
2020-05-27 17:32:12 +0530

    Text indentation.

--- HibernateHikariCPDemo/java/com/javalive/entity/Person.java
+++ HibernateHikariCPDemo/java/com/javalive/entity/Person.java
@@ -13,32 +13,32 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "PERSON")
 public class Person {
-  @Id
-  @GeneratedValue(strategy= GenerationType.IDENTITY)
-  @Column(name = "ID")
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
 
-  @Column(name = "NAME")
-  private String name;
+	@Column(name = "NAME")
+	private String name;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-   }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-   @Override
-   public String toString() {
-      return "Customer [id=" + id + ", name=" + name + "]";
-   }
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + "]";
+	}
 }
--- HibernateHikariCPDemo/java/com/javalive/hibernate/MainApp.java
+++ HibernateHikariCPDemo/java/com/javalive/hibernate/MainApp.java
@@ -10,29 +10,29 @@ import com.javalive.entity.Person;
  */
 public class MainApp {
 
-   public static void main(String[] args) {
-      Session session = null;
-      Transaction transaction = null;
-      try {
-         session = HibernateUtil.getSessionFactory().openSession();
-         transaction = session.getTransaction();
-         transaction.begin();
+	public static void main(String[] args) {
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.getTransaction();
+			transaction.begin();
 
-         Person person = new Person();
-         person.setName("Mike Lewis Jr");
-         session.save(person);
+			Person person = new Person();
+			person.setName("Mike Lewis Jr");
+			session.save(person);
 
-         transaction.commit();
-      } catch (Exception e) {
-         if (transaction != null) {
-            transaction.rollback();
-         }
-      } finally {
-         if (session != null) {
-            session.close();
-         }
-      }
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
 
-      HibernateUtil.shutdown();
-   }
+		HibernateUtil.shutdown();
+	}
 }
